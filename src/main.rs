6187f20bab76eb55c3ae1e6a7de2ab75f4ed3ba9
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-23 13:20:24 +0530

    Db connected

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use postgres::{Client, Error, NoTls};
 fn main() -> Result<(), Error> {
     println!("Hello, world!");
 
-    let mut client = Client::connect("postgresql://administrator:123@localhost/test", NoTls);
+    let mut client =
+        Client::connect("postgres://administrator:pass@localhost/test", NoTls).unwrap();
+
+    _ = client.batch_execute(
+        "
+            CREATE TABLE IF NOT EXISTS users (
+            id              SERIAL PRIMARY KEY,
+            username        VARCHAR UNIQUE NOT NULL,
+            password        VARCHAR NOT NULL,
+            email           VARCHAR UNIQUE NOT NULL
+            )
+    ",
+    );
+
     Ok(())
 }
