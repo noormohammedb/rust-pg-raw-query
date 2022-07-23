b8327c3db62eaad5b4932cbd2003ac614971157e
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-23 14:07:13 +0530

    read data from db

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,41 @@ fn main() -> Result<(), Error> {
     ",
     );
 
-    let res = client
-        .execute(
-            "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
-            &[&"user1", &"mypass", &"user@test.com"],
-        )
+    // let res = client
+    //     .execute(
+    //         "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
+    //         &[&"user1", &"mypass", &"user@test.com"],
+    //     )
+    //     .unwrap();
+
+    // client.execute(
+    //     "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
+    //     &[&"user2", &"mypass2", &"use2@gmail.com"],
+    // )?;
+    // client.execute(
+    //     "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
+    //     &[&"user3", &"anotherpass", &"mister3@test.com"],
+    // )?;
+
+    // dbg!(res);
+
+    let client_query_res = client
+        .query("SELECT id, username, password, email FROM app_user", &[])
         .unwrap();
 
-    client.execute(
-        "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
-        &[&"user2", &"mypass2", &"use2@gmail.com"],
-    )?;
-    client.execute(
-        "INSERT INTO app_user (username, password, email) VALUES ($1, $2, $3)",
-        &[&"user3", &"anotherpass", &"mister3@test.com"],
-    )?;
+    for row in client_query_res {
+        // let id: i32 = row.get(0).unwrap().get(0);
+        // let username: &str = row.get(0).unwrap().get(1);
+        // let password: &str = row.get(0).unwrap().get(2);
+        // let email: &str = row.get(0).unwrap().get(3);
 
-    dbg!(res);
+        let id: i32 = row.get(0);
+        let username: &str = row.get(1);
+        let password: &str = row.get(2);
+        let email: &str = row.get(3);
 
+        dbg!(id, username, password, email);
+        println!();
+    }
     Ok(())
 }
